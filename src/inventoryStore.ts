048d5bea4a92489f8a7aeaa61706f1d78e86cafc
Liberbell <liberbell@gmail.com>
2019-11-23 09:51:53 +0900

    inventoryStore6

--- src/inventoryStore.ts
+++ src/inventoryStore.ts
@@ -5,8 +5,8 @@ interface  Category     {
     }
 
 class InventoryStore {
-  _categories: Category[] = [];
-  _items: InventoryItem[] = [];
+  private _categories: Category[] = [];
+  private _items: InventoryItem[] = [];
   _isInitialized: Promise<boolean>;
 
   /** the inventory categories */
